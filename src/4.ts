05ceef7031f2c58984caeb25c2fc294352b78ad5
Denys Borysiuk <denisborisiuk@gmail.com>
2023-09-08 15:56:25 +0300

    add task-04

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: Person[] = [];
+  protected door: boolean = false;
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person has entered the house.');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is open.');
+    } else {
+      console.log('Invalid key.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +51,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
